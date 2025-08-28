6d8349f2235d1fb1124825e85edd8c76b17932fe
Owen Kunhardt <8000720+okunhardt@users.noreply.github.com>
2025-08-28 18:34:11 +0200

    Add baikal scripts.

---
+++ ct/baikal.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/okunhardt/ProxmoxScripts/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: bvdberg01
+# License: MIT | https://github.com/okunhardt/ProxmoxScripts/raw/main/LICENSE
+# Source: https://sabre.io/baikal/
+
+APP="Baikal"
+var_tags="${var_tags:-Dav}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-512}"
+var_disk="${var_disk:-4}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -d /opt/baikal ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  RELEASE=$(curl -fsSL https://api.github.com/repos/sabre-io/Baikal/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+  if [[ "${RELEASE}" != "$(cat ~/.baikal 2>/dev/null)" ]] || [[ ! -f ~/.baikal ]]; then
+    msg_info "Stopping Service"
+    systemctl stop apache2
+    msg_ok "Stopped Service"
+
+    msg_info "Backing up data"
+    mv /opt/baikal /opt/baikal-backup
+    msg_ok "Backed up data"
+
+    fetch_and_deploy_gh_release "baikal" "sabre-io/Baikal"
+    setup_composer
+
+    msg_info "Configuring Baikal"
+    cp -r /opt/baikal-backup/config/baikal.yaml /opt/baikal/config/
+    cp -r /opt/baikal-backup/Specific/ /opt/baikal/
+    chown -R www-data:www-data /opt/baikal/
+    chmod -R 755 /opt/baikal/
+    cd /opt/baikal
+    $STD composer install
+    msg_ok "Configured Baikal"
+
+    msg_info "Starting Service"
+    systemctl start apache2
+    msg_ok "Started Service"
+
+    msg_info "Cleaning up"
+    rm -rf /opt/baikal-backup
+    msg_ok "Cleaned"
+    msg_ok "Updated Successfully"
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}${CL}"
